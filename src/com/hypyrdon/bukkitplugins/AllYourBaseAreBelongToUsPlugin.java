90cb2fb615eaf6552640da8e57be1982a8f52ff8
hypyrdon <hypyrdon@localhost.loc>
2014-01-25 12:23:37 -0500

    initial commit

---
+++ src/com/hypyrdon/bukkitplugins/AllYourBaseAreBelongToUsPlugin.java
@@ -0,0 +1,10 @@
+package com.hypyrdon.bukkitplugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by hypyrdon on 1/25/14.
+ */
+public class AllYourBaseAreBelongToUsPlugin extends JavaPlugin{
+
+}
